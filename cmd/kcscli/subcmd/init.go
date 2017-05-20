58dc254aa1316454c4d367ff7c03e71efdb73aec
Nao Yonashiro <owan.orisano@gmail.com>
2017-05-21 01:11:08 +0900

    feat: implements 'kcscli init' command

--- cmd/kcscli/subcmd/init.go
+++ cmd/kcscli/subcmd/init.go
@@ -1,36 +1,58 @@
 package subcmd
 
 import (
-	"encoding/json"
-	"fmt"
 	"os"
 
+	"log"
+
+	"github.com/dustin/go-humanize"
 	"github.com/orisano/kcscli"
 	"github.com/spf13/cobra"
 )
 
+var (
+	humanizedTimeLimit   string
+	humanizedMemoryLimit string
+	author string
+)
+
 func init() {
+	initCmd.Flags().StringVar(&humanizedMemoryLimit, "memory-limit", "512MB", "problem's memory limit")
+	initCmd.Flags().StringVar(&humanizedTimeLimit, "time-limit", "3.0s", "problem's time limit")
+	initCmd.Flags().StringVar(&author, "author", "None", "author name")
 	RootCmd.AddCommand(initCmd)
 }
 
 var initCmd = &cobra.Command{
-	Use:   "init",
+	Use:   "init [problem_title]",
 	Short: "initialize kcs problem directory",
 	Long:  "initialize kcs problem directory",
 	Run: func(cmd *cobra.Command, args []string) {
-		if len(args) == 0 {
+		if len(args) != 1 {
 			cmd.Usage()
 			os.Exit(1)
 		}
+
+		timeLimit, _, err := humanize.ParseSI(humanizedTimeLimit)
+		if err != nil {
+			log.Fatal(err)
+			os.Exit(1)
+		}
+
+		memoryLimit, err := humanize.ParseBytes(humanizedMemoryLimit)
+		if err != nil {
+			log.Fatal(err)
+			os.Exit(1)
+		}
+
 		problem := kcscli.Problem{
 			Title:       args[0],
-			TimeLimit:   3.0,
-			MemoryLimit: 512,
-			Author:      "None",
+			TimeLimit:   timeLimit,
+			MemoryLimit: memoryLimit,
+			Author:      author,
 			Subtasks:    []kcscli.Subtask{},
 			Solvers:     []string{},
 		}
-		b, _ := json.MarshalIndent(&problem, "", "  ")
-		fmt.Println(string(b))
+		problem.Save(true)
 	},
 }
--- problem.go
+++ problem.go
@@ -1,5 +1,16 @@
 package kcscli
 
+import (
+	"encoding/json"
+	"io"
+	"io/ioutil"
+	"os"
+)
+
+const (
+	problemFilename = "problem.json"
+)
+
 // Subtask is component of kcs problem
 type Subtask struct {
 	Name  string   `json:"name"`
@@ -11,8 +22,43 @@ type Subtask struct {
 type Problem struct {
 	Title       string    `json:"title"`
 	TimeLimit   float64   `json:"time_limit"`
-	MemoryLimit int       `json:"memory_limit"`
+	MemoryLimit uint64    `json:"memory_limit"`
 	Author      string    `json:"author"`
 	Subtasks    []Subtask `json:"subtasks"`
 	Solvers     []string  `json:"solvers"`
+}
+
+func LoadProblem() (Problem, error) {
+	raw, err := ioutil.ReadFile(problemFilename)
+	if err != nil {
+		return Problem{}, err
+	}
+
+	var problem Problem
+	json.Unmarshal(raw, problem)
+	return problem, nil
+}
+
+func (p *Problem) Save(withStdout bool) error {
+	file, err := os.Create(problemFilename)
+	if err != nil {
+		return err
+	}
+
+	var writer io.Writer
+	if withStdout {
+		writer = io.MultiWriter(file, os.Stdout)
+	} else {
+		writer = file
+	}
+	return p.SaveTo(writer)
+}
+
+func (p *Problem) SaveTo(writer io.Writer) error {
+	b, err := json.MarshalIndent(p, "", "  ")
+	if err != nil {
+		return err
+	}
+	_, err = writer.Write(b)
+	return err
 }
